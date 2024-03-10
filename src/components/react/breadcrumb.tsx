b93fea1b51aca8583c3d7d5c70244c7730896028
shawn <shawnsandy04@gmail.com>
2024-03-10 13:21:24 -0400

    feat: add breadcrumb component
    Add a new Breadcrumb component in React. The component takes in routes and startRoute props to dynamically generate a breadcrumb navigation. Update the /posts/[page] page to use the new Breadcrumb component.

--- src/components/react/breadcrumb.tsx
+++ src/components/react/breadcrumb.tsx
@@ -1,10 +1,45 @@
+// Code: Breadcrumb component
 import React from "react";
 
-export const Breadcrumb = () => {
+type Crumbs = {
+  path?: string | number;
+  name?: string;
+};
+type BreadcrumbProps = {
+  routes?: Crumbs[];
+  startRoute?: React.ReactNode;
+};
+
+export const Breadcrumb: React.FC<BreadcrumbProps> = ({
+  startRoute = "Home",
+  routes,
+  ...props
+}) => {
+  const [breadcrumbs, setBreadcrumbs] = React.useState("/");
+  React.useEffect(() => {
+    const path = window.location.pathname;
+
+    setBreadcrumbs(path);
+  }, []);
+  const segments = breadcrumbs.split("/").filter((segment) => segment);
   return (
-    <div>
-      <p>Hello world</p>
-    </div>
+    <nav aria-label="Breadcrumb" {...props}>
+      <ul data-list="">
+        <li>
+          <a href="/">{startRoute}</a>
+        </li>
+        {segments.length &&
+          segments.map((segment: any, index) => (
+            <li key={index}>
+              <span>
+                <a href={`/${segments.slice(0, index + 1).join("/")}`}>
+                  {isNaN(segment) ? segment : <span>{`page ${segment}`}</span>}
+                </a>
+              </span>
+            </li>
+          ))}
+      </ul>{" "}
+    </nav>
   );
 };
 
