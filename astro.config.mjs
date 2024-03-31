ab3a15d6884fdb51d046f07f623f1da97fb40080
shawn <shawnsandy04@gmail.com>
2024-03-31 13:57:43 -0400

    chore: update dependencies and integrations
    - Update Astro, React, MDX and other integrations.
    - Add integrations for @keystatic/astro and @astrojs/markdoc.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,21 @@
-import { defineConfig } from "astro/config";
-import react from "@astrojs/react";
 import mdx from "@astrojs/mdx";
-import remarkToc from "remark-toc";
-import { rehypeAccessibleEmojis } from "rehype-accessible-emojis";
 import netlify from "@astrojs/netlify";
+import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
-import embeds from "astro-embed/integration";
 import spotlightjs from "@spotlightjs/astro";
-
+import embeds from "astro-embed/integration";
+import { defineConfig } from "astro/config";
+import { rehypeAccessibleEmojis } from "rehype-accessible-emojis";
+import keystatic from "@keystatic/astro";
 import lighthouse from "astro-lighthouse";
+import remarkToc from "remark-toc";
+
+import markdoc from "@astrojs/markdoc";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
-  integrations: [
-    react(),
-    sitemap(),
-    spotlightjs(),
-    lighthouse(),
-    embeds(),
-    mdx(),
-  ],
+  integrations: [react(), sitemap(), spotlightjs(), lighthouse(), embeds(), mdx(), keystatic(), markdoc()],
   adapter: netlify(),
   output: "hybrid",
   // Enable Custom Markdown options, plugins, etc.
@@ -30,7 +25,7 @@ export default defineConfig({
     rehypePlugins: [rehypeAccessibleEmojis],
     shikiConfig: {
       theme: "one-dark-pro",
-      wrap: true,
-    },
-  },
+      wrap: true
+    }
+  }
 });
