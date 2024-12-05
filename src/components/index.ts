19d699ead5ab08e83801a4824bb0b31b594eeb9e
shawn <shawnsandy04@gmail.com>
2024-12-05 07:08:18 -0500

    fix: fix export name

--- src/components/index.ts
+++ src/components/index.ts
@@ -5,6 +5,6 @@ export { default as Pagination } from "./astro/Pagination.astro";
 export { default as Blank } from "./astro/Blank.astro";
 export { default as Header } from "./astro/Header.astro";
 export { default as Footer } from "./astro/Footer.astro";
-export { default as PostList } from "./astro/PostsList.astro";
+export { default as PostsList } from "./astro/PostsList.astro";
 export { default as CollectionList } from "./astro/CollectionList.astro";
 export { default as ContactForm } from "./astro/ContactForm.astro";
