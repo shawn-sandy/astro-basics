042a290f8d8bd86d92edcc8f0a06fe47f756c137
shawn <shawnsandy04@gmail.com>
2024-12-12 22:31:11 -0500

    fix: remove spotlight

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import { rehypeAccessibleEmojis } from "rehype-accessible-emojis";
 import netlify from "@astrojs/netlify";
 import sitemap from "@astrojs/sitemap";
 import embeds from "astro-embed/integration";
-import spotlightjs from "@spotlightjs/astro";
+// import spotlightjs from "@spotlightjs/astro";
 import { astroImageTools } from "astro-imagetools";
 
 import lighthouse from "astro-lighthouse";
@@ -18,6 +18,7 @@ export default defineConfig({
     react(),
     sitemap(),
     lighthouse(),
+    // spotlightjs(),
     embeds(),
     mdx(),
     astroImageTools,
